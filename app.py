96fd8210bc2f13aacc9d7f385f2d96ec5de3a2af
oadams <amirumeir11@gmail.com>
2026-03-02 03:33:13 +0300

    Webhook test :rocket:

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from CI/CD!\n GET READY FOR THE BEST EXPERIENCE EVER!!! TEST AUTO DEPLOYMENT"
+    return "Hello from CI/CD!\n GET READY FOR THE BEST EXPERIENCE EVER!!! TEST AUTO DEPLOYMENT. TESTING A WEBHOOK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
