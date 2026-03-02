7459b326bd7476cdd89e86469c4388d3fac14288
oadams <amirumeir11@gmail.com>
2026-03-02 03:10:54 +0300

    update on app.py :rocket:

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from CI/CD!\n"
+    return "Hello from CI/CD!\n GET READY FOR THE BEST EXPERIENCE EVER!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
