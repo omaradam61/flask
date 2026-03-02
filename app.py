d60cf1c3628c1283ece79ae52fffd6de8723e4b9
oadams <amirumeir11@gmail.com>
2026-03-02 03:13:11 +0300

    auto deploy :rocket:

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from CI/CD!\n GET READY FOR THE BEST EXPERIENCE EVER!!!"
+    return "Hello from CI/CD!\n GET READY FOR THE BEST EXPERIENCE EVER!!! TEST AUTO DEPLOYMENT"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
